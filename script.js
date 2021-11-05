715c059c6208b37a2aee058024ecb71302897893
HongC03 <harrycheung03@gmail.com>
2021-11-05 23:52:15 +0800

    change start and reset button functionality

--- script.js
+++ script.js
@@ -1,60 +1,38 @@
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
-const resetButton = document.getElementById("reset");
-const startButton = document.getElementById("start");
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+const resetButton = document.getElementById('reset');
+const startButton = document.getElementById('start');
 
-const newYears = "5 Nov 2021 18:00:00";
+const newYears = '5 Nov 2021 23:30:00';
 
-startButton.addEventListener("click",()=>{
-    var x = setInterval(function() {
-        const newYearsDate = new Date(newYears);
-        const currentDate = new Date();
-    
-        
-    
-        const totalSeconds = (newYearsDate - currentDate) / 1000;
-    
-        console.log(newYearsDate )
-        console.log(totalSeconds)
-    
-        const days = Math.floor(totalSeconds / 3600 / 24);
-        const hours = Math.floor(totalSeconds / 3600) % 24;
-        const mins = Math.floor(totalSeconds / 60) % 60;
-        const seconds = Math.floor(totalSeconds) % 60;
-    
-        daysEl.innerHTML = days;
-        hoursEl.innerHTML = hours;
-        minsEl.innerHTML = mins;
-        secondsEl.innerHTML = seconds;
-    
-        if(totalSeconds <= 0){
-            clearInterval(x)
-            daysEl.innerHTML = 0;
-            hoursEl.innerHTML = 0;
-            minsEl.innerHTML = 0;
-            secondsEl.innerHTML = 0;
-            document.getElementById("dllm").innerHTML= ("u r ")
-        }    
-    },1000) 
-})
+let countDown = setInterval(Timer, 1000);
 
-resetButton.addEventListener("click",()=>{
-    clearInterval(x)
-    daysEl.innerHTML = 0;
-    hoursEl.innerHTML = 0;
-    minsEl.innerHTML = 0;
-    secondsEl.innerHTML = 0;
-    
-})
+function Timer() {
+	const newYearsDate = new Date(newYears);
+	const currentDate = new Date();
+	const totalSeconds = (newYearsDate - currentDate) / 1000;
 
+	const days = Math.floor(totalSeconds / 3600 / 24);
+	const hours = Math.floor(totalSeconds / 3600) % 24;
+	const mins = Math.floor(totalSeconds / 60) % 60;
+	const seconds = Math.floor(totalSeconds) % 60;
 
+	daysEl.innerText = days >= 0 ? days : 0;
+	hoursEl.innerText = hours >= 0 ? hours : 0;
+	minsEl.innerText = mins >= 0 ? mins : 0;
+	secondsEl.innerText = seconds >= 0 ? seconds : 0;
+}
 
+startButton.addEventListener('click', () => {
+	setInterval(Timer, 1000);
+});
 
-
-
-
-
-
-
+resetButton.addEventListener('click', () => {
+	clearInterval(countDown);
+	daysEl.innerText = 0;
+	hoursEl.innerText = 0;
+	minsEl.innerText = 0;
+	secondsEl.innerText = 0;
+});

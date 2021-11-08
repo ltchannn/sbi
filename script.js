8126b17a3f16704755c90f3bc7f956e966a96b11
ltchannn <90832696+ltchannn@users.noreply.github.com>
2021-11-08 12:11:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,34 +5,41 @@ const secondsEl = document.getElementById('seconds');
 const resetButton = document.getElementById('reset');
 const startButton = document.getElementById('start');
 
-const newYears = '5 Nov 2021 23:30:00';
+const newYears = '30 Nov 2021 24:00:00';
 
-let countDown = setInterval(Timer, 1000);
+let inIntervId;
 
-function Timer() {
-	const newYearsDate = new Date(newYears);
-	const currentDate = new Date();
-	const totalSeconds = (newYearsDate - currentDate) / 1000;
-
-	const days = Math.floor(totalSeconds / 3600 / 24);
-	const hours = Math.floor(totalSeconds / 3600) % 24;
-	const mins = Math.floor(totalSeconds / 60) % 60;
-	const seconds = Math.floor(totalSeconds) % 60;
-
-	daysEl.innerText = days >= 0 ? days : 0;
-	hoursEl.innerText = hours >= 0 ? hours : 0;
-	minsEl.innerText = mins >= 0 ? mins : 0;
-	secondsEl.innerText = seconds >= 0 ? seconds : 0;
+function startTimer() {
+    if (!inIntervId) {
+        inIntervId = setInterval(Timer, 1000)
+    }
 }
 
-startButton.addEventListener('click', () => {
-	setInterval(Timer, 1000);
-});
+function Timer() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-resetButton.addEventListener('click', () => {
-	clearInterval(countDown);
-	daysEl.innerText = 0;
-	hoursEl.innerText = 0;
-	minsEl.innerText = 0;
-	secondsEl.innerText = 0;
-});
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerText = days 
+    hoursEl.innerText = hours 
+    minsEl.innerText = mins 
+    secondsEl.innerText = seconds 
+}
+
+function stopTimer() {
+    clearInterval(inIntervId)
+    inIntervId = null;
+    daysEl.innerText = 0;
+    hoursEl.innerText = 0;
+    minsEl.innerText = 0;
+    secondsEl.innerText = 0;
+}
+
+startButton.addEventListener('click', startTimer);
+
+resetButton.addEventListener('click', stopTimer);
